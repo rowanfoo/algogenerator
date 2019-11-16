bebbc80168e265118c08ec7407a15671759eb49b
rowanfoo <rowanfoo@gmail.com>
2019-11-16 13:08:42 +0800

    multi concurrency

---
+++ src/main/java/com/dharma/algogenerator/service/Algo/AlgoSchedule.java
@@ -0,0 +1,20 @@
+package com.dharma.algogenerator.service.Algo;
+
+import com.dharma.algogenerator.controller.admin.AsxMetaStockImport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlgoSchedule {
+    @Autowired
+    AsxMetaStockImport asxMetaStockImport;
+
+    @Scheduled(cron = "0 15 15 ? * MON-FRI")
+    public void run() {
+
+        asxMetaStockImport.importAllData();
+
+    }
+
+}
