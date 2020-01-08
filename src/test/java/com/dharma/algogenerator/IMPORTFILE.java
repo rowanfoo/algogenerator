0e72743bf74377418fa90d53145062667d295db1
rowanfoo <rowanfoo@gmail.com>
2020-01-09 03:25:27 +0800

    add Fundamental

--- src/test/java/com/dharma/algogenerator/IMPORTFILE.java
+++ src/test/java/com/dharma/algogenerator/IMPORTFILE.java
@@ -39,7 +39,10 @@ public class IMPORTFILE {
     //EZY_CHART
     //ADD FILE PATH
 //    String FILEPATH = "E:\\TEMP\\HistoricalData-011018.txt";
-    String FILEPATH = "E:\\TEMP\\NHC.txt";
+//    String FILEPATH = "E:\\TEMP\\NHC.txt";
+//    String FILEPATH = "E:\\TEMP\\HistoricalData-2019-08-22.txt";
+//    String FILEPATH = "E:\\TEMP\\AKP.txt";
+    String FILEPATH = "E:\\TEMP\\ID8.txt";
     @Autowired
     ArrayList<String> allasxcodes;
 
@@ -55,16 +58,19 @@ public class IMPORTFILE {
 
                 // use comma as separator
                 String[] country = scanner.nextLine().split(" ");
+                //  System.out.println("-------------------------***********-------: " + country.length);
                 //System.out.println("-------------------------DATA----country---: "+ country[0] +  allcodes.contains( country[0].toUpperCase()));
-             //   System.out.println("-------------------------DATA----country---: "+allasxcodes);
+                //   System.out.println("-------------------------DATA----country---: "+allasxcodes);
                 if (allasxcodes.contains(country[0].toUpperCase())) {
+                    System.out.println("-------------------------DATA----country---: " + country[0].toUpperCase());
+
                     LocalDate date = LocalDate.parse(country[1], DateTimeFormatter.ofPattern("yyMMdd"));
 
                     CoreData data = new CoreData(country[0] + ".AX", date, new Double(country[5]) / 100, new Double("0"), new Double("0"), new Double(country[2]) / 100, new Double(country[3]) / 100,
                             new Double(country[4]) / 100, new Long(country[6]));
 
                     System.out.println("-------------------------DATA-------: " + data);
-                  datarepo.save(data);
+                    //               datarepo.save(data);
                     count++;
                 }
 
@@ -72,9 +78,9 @@ public class IMPORTFILE {
             }
 
             scanner.close();
-            System.out.println("-------------------------CLOSING-------SAVE: " + count);
-            datarepo.flush();
-            // calcChangePercent.run();
+            //   System.out.println("-------------------------CLOSING-------SAVE: " + count);
+            // datarepo.flush();
+            //  calcChangePercent.run();
             System.out.println("-------------------------CHG PERCENT RUN DONE-------: ");
 
             //  calcAverage.run();
@@ -95,6 +101,11 @@ public class IMPORTFILE {
 
         }
 
+    }
+
+    @Test
+    public void calcPercent() {
+        calcChangePercent.run();
     }
 
 }
